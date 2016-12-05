cb5ea66ce01fdc0ae926394888403e66d1d5d8b3
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-12-05 16:30:30 -0500

    Start of asteroid plotter but I need a way to propogate first

---
+++ plot_asteroid.py
@@ -0,0 +1,21 @@
+# script to plot the orbits of several asteroids
+from planet_coe import *
+import numpy as np
+
+# load the COEs for the asteroids
+# 2008 EV5
+coe_ev5 = (0.9582899238313918*(1 - 0.08348599378460778**2),
+    0.08348599378460778,np.deg2rad(7.436787362690259),
+    np.deg2rad(93.39122898787916),np.deg2rad(234.8245876826614),
+    nu)
+# Itokawa
+
+# Bennu
+
+# plot all the planets
+
+# plot each asteroid
+
+# convert COE to RV
+
+# transform RV to Sun Earth 3BP and nondimensionalize
