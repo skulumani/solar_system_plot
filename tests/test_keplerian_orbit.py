7b8ebd6fc3fa8144b7ccb89c8487a0b65c785258
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-12-05 17:05:17 -0500

    Initial framework for testing

--- tests/test_keplerian_orbit.py
+++ tests/test_keplerian_orbit.py
@@ -2,7 +2,11 @@
 import unittest
 
 class orbital_elements(unittest.TestCase):
+    # convert from \nu to E,M and vice versa
+    known_anomalies_M2Ev = (
 
-
+        )
+    known_anomalies_v2EM = ()
+    
 if __name__ == "__main__":
     unittest.main()
