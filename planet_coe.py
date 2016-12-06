30dc7d0c3ad2c314c18838e2c465c72b6c04ff6f
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-12-06 10:43:30 -0500

    Try to seperate between library and executable files

--- planet_coe.py
+++ planet_coe.py
@@ -1,7 +1,4 @@
 import numpy as np 
-from keplerian_orbit import *
-from utilities import *
-from time_utilities import *
 
 def element_approx(JD_curr, planet_flag):
     """
@@ -256,30 +253,8 @@ def plot_planets(JD):
     plt.show()
 
 if __name__ == "__main__":
+    print('This works')
 
-    from datetime import datetime
-
-    today_date = datetime.today()
-    yr = today_date.year
-    mon = today_date.month
-    day = today_date.day
-    hr = today_date.hour
-    minute = today_date.minute
-    sec = today_date.second
-
-    JD_curr,MJD = date2jd(yr,mon,day,hr,minute,sec)
-    planet_flag = 3
-    p,ecc,inc,raan,argp,nu = element_approx(JD_curr,planet_flag)
-
-    print("p: %16.16f au" % p)
-    print("a: %16.16f au" % (p/(1-ecc**2)))
-    print("ecc: %16.16f " % ecc)
-    print("inc: %16.16f deg" % np.rad2deg(inc))
-    print("raan: %16.16f deg" % np.rad2deg(raan))
-    print("argp: %16.16f deg" % np.rad2deg(argp))
-    print("nu: %16.16f deg" % np.rad2deg(nu))
-
-    plot_planets(JD_curr)
 # Table 2a.
 
 # Keplerian elements and their rates, with respect to the mean ecliptic and equinox of J2000,
