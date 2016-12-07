fe6fc390deb9b4c5b54a2727ef9e9bfe8a58ca08
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-12-07 13:39:22 -0500

    Modify plotting and output RV state to terminal

--- plot_asteroid.py
+++ plot_asteroid.py
@@ -24,11 +24,11 @@ minute = today_date.minute
 sec = today_date.second
 
 JD_curr,MJD = date2jd(yr,mon,day,hr,minute,sec)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
 # plot the planets
 planet_names = ('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto')
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 # loop over all the planets and compute the COE
 for planet_flag in range(4):
     # calculate the conic orbit for each planet 
@@ -39,11 +39,7 @@ for planet_flag in range(4):
     ax.plot(x,y,z,'b')
     ax.plot([xs],[ys],[zs],'ro')
     ax.text(xs,ys,zs,planet_names[planet_flag])
-
-ax.set_xlim([-5,5])
-ax.set_ylim([-5,5])
-ax.set_zlim([-5,5])
-plt.axis('square')    
+   
 
 asteroid_names = ('EV5','Itokawa','Bennu')
 # plot each asteroid
@@ -55,7 +51,31 @@ for ast_flag in range(3):
     ax.plot([xs],[ys],[zs],'ro')
     ax.text(xs,ys,zs,asteroid_names[ast_flag])
 
-# convert COE to RV
+    # convert COE to RV and print to screen
+    # make sure the units of the COEs are consistent
+    p = 1/km2au * p # convert to km
+    mu = 1.32712440018e11 # km^3/sec^2
 
+    r_ijk, v_ijk, r_pqw, v_pqw = coe2rv(p,ecc,inc,raan,argp,nu,mu)
+
+    # print to screen/text file
+    print("%s state wrt Sol barycenter" % asteroid_names[ast_flag])
+    print("    Pos (km): %16.16f %16.16f %16.16f" % (r_ijk[0], r_ijk[1], r_ijk[2]))
+    print("    Vel (km/sec): %16.16f %16.16f %16.16f" % (v_ijk[0], v_ijk[1], v_ijk[2]))
+# print the state of the Earth
+p,ecc,inc,raan,argp,nu = planet_coe(JD_curr,3)
+r_ijk, v_ijk, r_pqw, v_pqw = coe2rv(p*1/km2au,ecc,inc,raan,argp,nu,mu)
+
+print("")
+print("Earth State wrt sol barycenter")
+print("    Pos (km): %16.16f %16.16f %16.16f" % (r_ijk[0], r_ijk[1], r_ijk[2]))
+print("    Vel (km/sec): %16.16f %16.16f %16.16f" % (v_ijk[0], v_ijk[1], v_ijk[2]))
+
+ax.set_xlim([-5,5])
+ax.set_ylim([-5,5])
+ax.set_zlim([-5,5])
+ax.set_title('Solar System JD %10.3f' % JD_curr)
+plt.axis('off')
+ax.view_init(azim=0, elev=90)
 plt.show()
 # transform RV to Sun Earth 3BP and nondimensionalize
