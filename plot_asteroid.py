bc17d62bf4195a8773925da8f11f2851f42a129f
Shankar Kulumani <shanks.k@gmail.com>
2016-12-06 20:37:18 -0500

    Now we can plot a few asteroids

--- plot_asteroid.py
+++ plot_asteroid.py
@@ -1,52 +1,57 @@
 # script to plot the orbits of several asteroids
-import numpy as np
+import numpy as np 
+from datetime import datetime
+from utilities.time import date2jd
+from utilities.attitude import normalize
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+from keplerian_orbit.keplerian_orbit import conic_orbit, kepler_eq_E  
+from orbital_elements.planet_coe import planet_coe
 
-# load the COEs for the asteroids
-# 2008 EV5
-coe_ev5 = (0.9582899238313918*(1 - 0.08348599378460778**2),
-    0.08348599378460778,np.deg2rad(7.436787362690259),
-    np.deg2rad(93.39122898787916),np.deg2rad(234.8245876826614),
-    np.deg2rad(90))
-# Itokawa
-
-# Bennu
+from orbital_elements.asteroid_coe import asteroid_coe
 
 # plot all the planets
+today_date = datetime.today()
+yr = today_date.year
+mon = today_date.month
+day = today_date.day
+hr = today_date.hour
+minute = today_date.minute
+sec = today_date.second
 
+JD_curr,MJD = date2jd(yr,mon,day,hr,minute,sec)
+
+# plot the planets
+planet_names = ('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# loop over all the planets and compute the COE
+for planet_flag in range(4):
+    # calculate the conic orbit for each planet 
+    p,ecc,inc,raan,argp,nu = planet_coe(JD_curr,planet_flag)
+
+    (x,y,z,xs,ys,zs) = conic_orbit(p,ecc, inc, raan, argp, nu, nu)
+    # plot the planet to figure
+    ax.plot(x,y,z,'b')
+    ax.plot([xs],[ys],[zs],'ro')
+    ax.text(xs,ys,zs,planet_names[planet_flag])
+
+ax.set_xlim([-5,5])
+ax.set_ylim([-5,5])
+ax.set_zlim([-5,5])
+plt.axis('square')    
+
+asteroid_names = ('EV5','Itokawa','Bennu')
 # plot each asteroid
+for ast_flag in range(3):
+    (p,ecc,inc,raan,argp,nu) = asteroid_coe(JD_curr,ast_flag)
+
+    (x,y,z,xs,ys,zs) = conic_orbit(p,ecc, inc, raan, argp, nu, nu)
+    ax.plot(x,y,z,'g')
+    ax.plot([xs],[ys],[zs],'ro')
+    ax.text(xs,ys,zs,asteroid_names[ast_flag])
 
 # convert COE to RV
 
-# transform RV to Sun Earth 3BP and nondimensionalize
-
-
-import matplotlib.pyplot as plt 
-from mpl_toolkits.mplot3d import Axes3D
-import keplerian_orbit.keplerian_orbit as kpl
-
-# test Kepler Equation solver
-M_in = np.array(0.5)
-ecc_in = np.array(0)
-E_out, nu_out, count_out = kpl.kepler_eq_E(M_in,ecc_in)
-
-print("E: %5.4f M: %5.4f count: %5.4f" %( E_out,nu_out,count_out))
-
-# define orbital elements
-p = 6578
-ecc = 0.2
-inc = np.deg2rad(10)
-raan = np.deg2rad(12)
-argp = np.deg2rad(15)
-nu = np.deg2rad(165)
-# call conic orbit plotter
-(x,y,z,xs,ys,zs) = kpl.conic_orbit(p,ecc, inc, raan, argp, nu, nu)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(x,y,z,'b')
-ax.plot([xs],[ys],[zs],'ro')
-plt.axis('equal')
 plt.show()
-
-# test True anomaly converter
-print("Convert \\nu to E and M")
+# transform RV to Sun Earth 3BP and nondimensionalize
