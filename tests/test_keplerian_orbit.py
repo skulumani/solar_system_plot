3cb4e48e0ada2ba799b8e45f6d1c361133c447c1
Shankar Kulumani <shanks.k@gmail.com>
2016-12-21 10:55:09 -0500

    Add some more tests for keplerian orbit functions

--- tests/test_keplerian_orbit.py
+++ tests/test_keplerian_orbit.py
@@ -1,5 +1,5 @@
 """Pytest for keplerian_orbit.py"""
-from keplerian_orbit.keplerian_orbit import kepler_eq_E
+from keplerian_orbit.keplerian_orbit import kepler_eq_E, nu2anom 
 import numpy as np
 
 # define an Earth GEO stationary orbit
@@ -16,6 +16,74 @@ def test_kepler_eq_E():
     E_python, nu_python, count_python = kepler_eq_E(M,ecc)
 
     np.testing.assert_allclose((E_python, nu_python),(E_matlab,nu_matlab))
+
+def test_kepler_eq_E_zero():
+    """
+        Make sure that at zero nu=E=M
+    """
+    M = 0.0
+    ecc = 1.2
+    E_true = 0.0
+    nu_true = 0.0
+
+    E_python, nu_python, count_python = kepler_eq_E(M,ecc)
+
+    np.testing.assert_array_almost_equal((E_python,nu_python),(E_true,nu_true))
+
+
+def test_kepler_eq_E_pi():
+    """
+        Make sure that at pi nu=E=M
+    """
+    M = np.pi
+    ecc = 0.9
+    E_true = np.pi
+    nu_true = np.pi
+
+    E_python, nu_python, count_python = kepler_eq_E(M,ecc)
+
+    np.testing.assert_array_almost_equal((E_python,nu_python),(E_true,nu_true))
+
+def test_nu2anom():
+    """
+        A matlab test case which is copied here
+    """
+    M_matlab = np.deg2rad(110)
+    ecc = 0.9
+    E_matlab = 2.475786297687611 
+    nu_matlab = 2.983273149717047
+
+    E_python, M_python = nu2anom(nu_matlab,ecc)
+
+    np.testing.assert_allclose((E_python, M_python),(E_matlab,M_matlab))
+
+def test_nu2anom_zero():
+    """
+        Make sure that at zero nu=E=M
+    """
+    M_true = 0.0
+    ecc = 1.2
+    E_true = 0.0
+    nu_true = 0.0
+
+    E_python, M_python = nu2anom(nu_true,ecc)
+
+    np.testing.assert_allclose((E_python, M_python),(E_true,M_true))
+
+
+def test_nu2anom_pi():
+    """
+        Make sure that at pi nu=E=M
+    """
+    M_true = np.pi
+    ecc = 0.9
+    E_true = np.pi
+    nu_true = np.pi
+    
+    E_python, M_python = nu2anom(nu_true,ecc)
+
+    np.testing.assert_allclose((E_python, M_python),(E_true,M_true))
+
 
 def test_tof_delta_t():
     """Test propogation using Kepler's Eq"""
